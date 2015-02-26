685ebe5d18f8fe166c968851013caf1996cd9313
lee <lee@gmail>
2015-02-26 23:01:44 +0800

    init

---
+++ Assistant/DynamicParameter/DynamicParameterCreator.ps1
@@ -0,0 +1,69 @@
+﻿function New-RuntimeDefinedParameter
+{
+   param(
+   [string]$parameterType,
+   [Parameter(Mandatory=$true)]
+   [string]$parameterName,
+   [scriptblock]$ParameterAttributeScript
+   )
+   $attributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
+
+   function New-ParameterAttribute
+    {
+       param(
+       [string]$ParameterSet,
+       [switch]$Mandatory,
+       [switch]$ValueFromPipeline
+       )
+
+       $attr = New-Object System.Management.Automation.ParameterAttribute
+
+       if ($ParameterSet)
+       {
+           $attr.ParameterSetName = $ParameterSet
+       }
+
+       if($Mandatory)
+       {
+           $attr.Mandatory = $true
+       }
+
+       if ($ValueFromPipeline)
+       {
+           $attr.ValueFromPipeline = $true
+       }
+
+       $attributeCollection.Add($attr)
+    }
+
+    & $ParameterAttributeScript
+
+    $dynParameter = NewObject System.Management.Automation.RuntimeDefinedParameter  $parameterName,$parameterName, $attributeCollection
+}
+
+
+function New-ParameterAttribute
+{
+   param(
+   [string]$ParameterSet,
+   [switch]$Mandatory,
+   [switch]$ValueFromPipeline
+   )
+
+   $attr = New-Object System.Management.Automation.ParameterAttribute
+
+   if ($ParameterSet)
+   {
+       $attr.ParameterSetName = $ParameterSet
+   }
+
+   if($Mandatory)
+   {
+       $attr.Mandatory = $true
+   }
+
+   if ($ValueFromPipeline)
+   {
+       $attr.ValueFromPipeline = $true
+   }
+}
---
+++ Snippets/funtionSnippets.ps1
@@ -0,0 +1,24 @@
+﻿New-IseSnippet -Title "fn" -Description "create new function" -Text @"
+function Name
+{
+   
+}
+"@ -CaretOffset 9
+
+New-IseSnippet -Title "fn-p" -Description "create new function" -Text @"
+function Name
+{
+   param(
+
+   )
+
+}
+"@ -CaretOffset 9
+
+New-IseSnippet -Title "vs" -Description "ValidatSet Attribute" -Text "[ValidateSet()]" -CaretOffset 13
+New-IseSnippet -Title "p" -Description "Parameter Attribute" -Text "[Parameter()]" -CaretOffset 11
+New-IseSnippet -Title "pp" -Description "Parameter Attribute + ValueFromPipeline" -Text "[Parameter(ValueFromPipeline=`$true)]" -CaretOffset 34
+#New-IseSnippet -Title  "pos" -Description "Position Property" -Text "Position"
+
+
+
