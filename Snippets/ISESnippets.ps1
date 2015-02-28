2fced036ca9afce1d0ed2799ddafcd466d639f8f
lee <lee@gmail>
2015-03-01 03:34:51 +0800

    Add: Ise Snippets

---
+++ Snippets/ISESnippets.ps1
@@ -0,0 +1,7 @@
+﻿#caret
+New-IseSnippet -Title "caret" -Description "Insert Get-Caret" -Text "(Get-Caret)" -CaretOffset 11
+New-IseSnippet -Title "caret-a" -Description "Insert Get-Caret array formate" -Text "(Get-Caret -Format array)" -CaretOffset 25
+
+
+#Add-On
+New-IseSnippet -Title "AddOn" -Description "Insert Add-On" -Text "`$psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Add(" -CaretOffset 52
