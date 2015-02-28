513ed0b47fe5f19f667503c0b45d78ecfdd41aa6
lee <lee@gmail>
2015-02-28 16:40:28 +0800

    Add ISE Add-On: fresh function definition
    ctrl+shift+space

---
+++ ISE/AutoFreshFunctionDefinition.ps1
@@ -0,0 +1,21 @@
+﻿# when you want fresh a function's definition:
+# put your caret inside the definition of the function, press "ctrl+shift+space"
+
+#this module require:
+# PSAstCore.ps1
+
+$psISE.CurrentPowerShellTab.AddOnsMenu.Submenus.Add("run definition", {
+
+     $currentAst =  New-PsAst -text $psISE.CurrentFile.Editor.Text
+    
+    $caretLineNumber = $psISE.CurrentFile.Editor.CaretLine
+    $caretColumNumber = $psISE.CurrentFile.Editor.CaretColumn
+
+    $functionAst =  ($currentAst | findAst -type FunctionDefinition -Depth First -contains $caretLineNumber,$caretColumNumber)
+
+    if ($functionAst -ne $null)
+    {
+        Invoke-Expression $functionAst.ToString()
+    }
+
+ },"ctrl+shift+space")  
