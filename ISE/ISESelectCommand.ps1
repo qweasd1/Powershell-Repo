9c77a1d06cb79cc4a98288ff3c6cfa7f3052832e
lee <lee@gmail>
2015-02-28 17:13:33 +0800

    init: Select-PsAst

---
+++ ISE/ISESelectCommand.ps1
@@ -0,0 +1,15 @@
+﻿
+#select input PSAst in current file
+function Select-PSAst
+{
+   param(
+   [Parameter(ValueFromPipeline=$true)]
+   [System.Management.Automation.Language.Ast]$PsAst
+   )
+   Write-Verbose "StartLineNumber : $($PsAst.Extent.StartLineNumber)"
+   Write-Verbose "StartColumnNumber : $($PsAst.Extent.StartColumnNumber)"
+   Write-Verbose "EndLineNumber : $($PsAst.Extent.EndLineNumber)"
+   Write-Verbose "EndColumnNumber : $($PsAst.Extent.EndColumnNumber)"
+
+   $psISE.CurrentFile.Editor.Select($PsAst.Extent.StartLineNumber,$PsAst.Extent.StartColumnNumber, $PsAst.Extent.EndLineNumber,$PsAst.Extent.EndColumnNumber)
+}
