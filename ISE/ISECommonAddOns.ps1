84eea225f065f6ee99e4f179261a687901985d03
lee <lee@gmail>
2015-03-02 21:57:40 +0800

    unknown

---
+++ ISE/ISECommonAddOns.ps1
@@ -0,0 +1,32 @@
+﻿#dependency:
+,"D:\Repository\Powershell\ISE\ISEBasicCmdlets.ps1"
+
+function comment
+{
+    $selectedLines = (Get-ISESelectedText) -split $RegexNewLine
+
+    $result = $selectedLines | Select-String -Pattern "^\s*#"
+    if ($result)
+    {
+        # has comment
+        $noCommentLines = $selectedLines | %{$_ -replace "^\s*#",""}
+
+        $noCommentText = $noCommentLines -join $NewLine
+
+        Insert-ISEText $noCommentText
+    }
+    else
+    {
+        # no comment
+        $commentLines = $selectedLines | %{ if($_ -notmatch "^\s*$") {"#$_"}else{$_} }
+        $commentText = $commentLines -join $NewLine
+
+        Insert-ISEText $commentText
+    }
+    
+}
+
+
+#12
+#22
+#33
